5d22ff4b173b7616259500c71659136f750b8b9e
hamadoun Sankare <husumaci@gmail.com>
2022-10-26 14:34:41 +0000

    dropdown added

--- src/components/shared/TextInput.tsx
+++ src/components/shared/TextInput.tsx
@@ -2,13 +2,22 @@ import React, { ReactNode } from "react";
 import { BoxProps, TextProps } from "@shopify/restyle";
 import { EvilIcons } from "@expo/vector-icons";
 import { TextInput } from "react-native";
+
+import { Picker } from "@react-native-picker/picker";
 import { Theme } from "../../theme";
 import Box from "./Box";
 
+enum InputType {
+  text,
+  dropdown,
+}
+
 type InputProps = {
   icon?: ReactNode;
+  type?: keyof typeof InputType;
   placeholder?: string;
   value: string;
+  dropdownValues?: string[];
   onChange: () => void;
 } & Partial<BoxProps<Theme>>;
 
@@ -17,6 +26,8 @@ const Input: React.FC<InputProps> = ({
   icon,
   value,
   onChange,
+  type,
+  dropdownValues,
   ...props
 }) => {
   return (
@@ -31,12 +42,20 @@ const Input: React.FC<InputProps> = ({
       height={50}
       {...props}
     >
-      <TextInput
-        onChangeText={onChange}
-        value={value}
-        placeholder={placeholder}
-        style={{width:"100%",height:"100%"}}
-      />
+      {type == "text" ? (
+        <TextInput
+          onChangeText={onChange}
+          value={value}
+          placeholder={placeholder}
+          style={{ width: "100%", height: "100%" }}
+        />
+      ) : (
+        <Picker selectedValue={value} onValueChange={onChange}>
+          {dropdownValues?.map((item, index) => (
+            <Picker.Item key={index} label={item} value={item} />
+          ))}
+        </Picker>
+      )}
     </Box>
   );
 };
